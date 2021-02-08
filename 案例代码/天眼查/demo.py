290927257e9f55b9c13be8a599f99583a333c39c
lixi5338619 <125066648@qq.com>
2021-02-08 10:54:00 +0800

    add 天眼查demo

---
+++ 案例代码/天眼查/demo.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+# 96个
+
+industry_href_list = ["https://www.tianyancha.com/search/oc45", "https://www.tianyancha.com/search/oc44",
+                      "https://www.tianyancha.com/search/oc46", "https://www.tianyancha.com/search/oc49",
+                      "https://www.tianyancha.com/search/oc48", "https://www.tianyancha.com/search/oc47",
+                      "https://www.tianyancha.com/search/oc50", "https://www.tianyancha.com/search/oc51",
+                      "https://www.tianyancha.com/search/oc52", "https://www.tianyancha.com/search/oc59",
+                      "https://www.tianyancha.com/search/oc58", "https://www.tianyancha.com/search/oc57",
+                      "https://www.tianyancha.com/search/oc56", "https://www.tianyancha.com/search/oc55",
+                      "https://www.tianyancha.com/search/oc60", "https://www.tianyancha.com/search/oc53",
+                      "https://www.tianyancha.com/search/oc54", "https://www.tianyancha.com/search/oc04",
+                      "https://www.tianyancha.com/search/oc05", "https://www.tianyancha.com/search/oc01",
+                      "https://www.tianyancha.com/search/oc02", "https://www.tianyancha.com/search/oc03",
+                      "https://www.tianyancha.com/search/oc11", "https://www.tianyancha.com/search/oc12",
+                      "https://www.tianyancha.com/search/oc08", "https://www.tianyancha.com/search/oc09",
+                      "https://www.tianyancha.com/search/oc06", "https://www.tianyancha.com/search/oc07",
+                      "https://www.tianyancha.com/search/oc10", "https://www.tianyancha.com/search/oc35",
+                      "https://www.tianyancha.com/search/oc36", "https://www.tianyancha.com/search/oc33",
+                      "https://www.tianyancha.com/search/oc34", "https://www.tianyancha.com/search/oc39",
+                      "https://www.tianyancha.com/search/oc37", "https://www.tianyancha.com/search/oc38",
+                      "https://www.tianyancha.com/search/oc43", "https://www.tianyancha.com/search/oc42",
+                      "https://www.tianyancha.com/search/oc41", "https://www.tianyancha.com/search/oc40",
+                      "https://www.tianyancha.com/search/oc22", "https://www.tianyancha.com/search/oc23",
+                      "https://www.tianyancha.com/search/oc24", "https://www.tianyancha.com/search/oc25",
+                      "https://www.tianyancha.com/search/oc26", "https://www.tianyancha.com/search/oc27",
+                      "https://www.tianyancha.com/search/oc28", "https://www.tianyancha.com/search/oc29",
+                      "https://www.tianyancha.com/search/oc30", "https://www.tianyancha.com/search/oc32",
+                      "https://www.tianyancha.com/search/oc31", "https://www.tianyancha.com/search/oc19",
+                      "https://www.tianyancha.com/search/oc17", "https://www.tianyancha.com/search/oc18",
+                      "https://www.tianyancha.com/search/oc15", "https://www.tianyancha.com/search/oc16",
+                      "https://www.tianyancha.com/search/oc13", "https://www.tianyancha.com/search/oc14",
+                      "https://www.tianyancha.com/search/oc21", "https://www.tianyancha.com/search/oc20",
+                      "https://www.tianyancha.com/search/oc71", "https://www.tianyancha.com/search/oc72",
+                      "https://www.tianyancha.com/search/oc73", "https://www.tianyancha.com/search/oc74",
+                      "https://www.tianyancha.com/search/oc75", "https://www.tianyancha.com/search/oc78",
+                      "https://www.tianyancha.com/search/oc77", "https://www.tianyancha.com/search/oc76",
+                      "https://www.tianyancha.com/search/oc79", "https://www.tianyancha.com/search/oc80",
+                      "https://www.tianyancha.com/search/oc81", "https://www.tianyancha.com/search/oc62",
+                      "https://www.tianyancha.com/search/oc61", "https://www.tianyancha.com/search/oc64",
+                      "https://www.tianyancha.com/search/oc65", "https://www.tianyancha.com/search/oc63",
+                      "https://www.tianyancha.com/search/oc67", "https://www.tianyancha.com/search/oc66",
+                      "https://www.tianyancha.com/search/oc69", "https://www.tianyancha.com/search/oc68",
+                      "https://www.tianyancha.com/search/oc70", "https://www.tianyancha.com/search/oc96",
+                      "https://www.tianyancha.com/search/oc83", "https://www.tianyancha.com/search/oc84",
+                      "https://www.tianyancha.com/search/oc82", "https://www.tianyancha.com/search/oc95",
+                      "https://www.tianyancha.com/search/oc94", "https://www.tianyancha.com/search/oc93",
+                      "https://www.tianyancha.com/search/oc92", "https://www.tianyancha.com/search/oc91",
+                      "https://www.tianyancha.com/search/oc90", "https://www.tianyancha.com/search/oc88",
+                      "https://www.tianyancha.com/search/oc89", "https://www.tianyancha.com/search/oc86",
+                      "https://www.tianyancha.com/search/oc87", "https://www.tianyancha.com/search/oc85"]
+
+province_list = ['?base=bj', '?base=sh', '?base=tj', '?base=cq', '?base=heb', '?base=sx', '?base=nmg', '?base=ln',
+                 '?base=jl', '?base=hlj', '?base=js', '?base=zj', '?base=ah', '?base=fj', '?base=jx', '?base=sd',
+                 '?base=hen', '?base=hub', '?base=hun', '?base=gd', '?base=gx', '?base=sc', '?base=gz', '?base=yn',
+                 '?base=han', '?base=snx', '?base=gs', '?base=nx', '?base=qh', '?base=xj', '?base=xz', '?base=hk',
+                 '?base=mo', '?base=tw']
+
+industry_name_list = ["燃气生产和供应业", "电力、热力生产和供应业", "水的生产和供应业", "建筑安装业", "土木工程建筑业", "房屋建筑业", "建筑装饰和其他建筑业", "批发业", "零售业",
+                      "仓储业", "装卸搬运和运输代理业", "管道运输业", "航空运输业", "水上运输业", "邮政业", "铁路运输业", "道路运输业", "渔业", "农、林、牧、渔服务业", "农业",
+                      "林业", "畜牧业", "开采辅助活动", "其他采矿业", "黑色金属矿采选业", "有色金属矿采选业", "煤炭开采和洗选业", "石油和天然气开采业", "非金属矿采选业",
+                      "专用设备制造业", "汽车制造业", "金属制品业", "通用设备制造业", "计算机、通信和其他电子设备制造业", "铁路、船舶、航空航天和其他运输设备制造业", "电气机械和器材制造业",
+                      "金属制品、机械和设备修理业", "废弃资源综合利用业", "其他制造业", "仪器仪表制造业", "造纸和纸制品业", "印刷和记录媒介复制业", "文教、工美、体育和娱乐用品制造业",
+                      "石油加工、炼焦和核燃料加工业", "化学原料和化学制品制造业", "医药制造业", "化学纤维制造业", "橡胶和塑料制品业", "非金属矿物制品业", "有色金属冶炼和压延加工业",
+                      "黑色金属冶炼和压延加工业", "皮革、毛皮、羽毛及其制品和制鞋业", "纺织业", "纺织服装、服饰业", "酒、饮料和精制茶制造业", "烟草制品业", "农副食品加工业", "食品制造业",
+                      "家具制造业", "木材加工和木、竹、藤、棕、草制品业", "租赁业", "商务服务业", "研究和试验发展", "专业技术服务业", "科技推广和应用服务业", "公共设施管理业",
+                      "生态保护和环境治理业", "水利管理业", "居民服务业", "机动车、电子产品和日用产品修理业", "其他服务业", "餐饮业", "住宿业", "互联网和相关服务",
+                      "软件和信息技术服务业", "电信、广播电视和卫星传输服务", "资本市场服务", "货币金融服务", "其他金融业", "保险业", "房地产业", "国际组织", "卫生", "社会工作",
+                      "教育", "基层群众自治组织", "群众团体、社会团体和其他成员组织", "社会保障", "人民政协、民主党派", "国家机构", "中国共产党机关", "体育", "娱乐业",
+                      "广播、电视、电影和影视录音制作业", "文化艺术业", "新闻和出版业"]
+
+
+from bs4 import BeautifulSoup
+import requests
+from lxml import etree
+
+
+def get_html(url):
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Cache-Control": "max-age=0",
+        "Connection": "keep-alive",
+        "Host": "www.tianyancha.com",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        return response.text
+    except BaseException:
+        print("请求异常")
+        return
+
+def get_info(url):
+    html = get_html(url)
+    # 企业简介
+    e = etree.HTML(html)
+    # 解析就自己写吧
+    print(e.xpath("//div[@class='detail-content']//text()"))
+
+
+# 根据搜索页获得公司
+def get_page_company(page_url):
+    html = get_html(page_url)
+    soup = BeautifulSoup(html, 'html.parser')
+    # 页内公司href 链表
+    company_href_list = []
+    company_list_div = soup.find('div', class_="result-list sv-search-container")
+    no_company_div = soup.find('div', class_="no-result-container deep-search-detail")
+    # 此条件下确实没有分页信息或者公司
+    if no_company_div is not None:
+        return True
+    # 如果没有上面的条件，说明被反爬挡住了
+    if company_list_div is None:
+        print("此页查找不到分页")
+        return get_page_company(page_url)
+    a_list = company_list_div.find_all('a')
+    if a_list is not None:
+        for item in a_list:
+            if 'https://www.tianyancha.com/company/' in str(item.get("href")):
+                if len(str(item.get("href"))) > 36:
+                    return str(item.get("href"))
+
+
+def get_page_city(province_href):
+    html = get_html(province_href)
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find('div', class_="scope-box")
+    if div is None:
+        print("此页查找不到市区内容:", province_href)
+        return get_page_city(province_href)
+    a_list = div.find_all('a')
+
+    for city_a in a_list:
+        city_href = city_a.get('href')
+        # city_href   某省某行业，不同区的企业
+        # 这里可以把 city_href存到redis当任务发布，增加采集效率
+        url = get_page_company(city_href)  # 获取列表页的detail_link
+        get_info(url)
+        break # 测试所以 break
+
+# TODO 很简单的走了一遍流程，想高效采集需要大量代理ip和多线程或者分布式采集。
+
+for industry_href in industry_href_list:
+    for province_str in province_list:
+        province_href = industry_href + province_str
+        get_page_city(province_href)
+        break
+    break   # 测试所以 break
+
